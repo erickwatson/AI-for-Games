29835840186beeac31936e37565771e1c5c3a946
Erick Watson <fredmackay@hotmail.com>
2018-06-20 16:41:21 +0930

    Math for Games
    Included MAthLib for extra fucntions

--- project2D/FollowBehaviour.cpp
+++ project2D/FollowBehaviour.cpp
@@ -12,9 +12,18 @@ FollowBehaviour::~FollowBehaviour()
 }
 
 eBehaviourResult FollowBehaviour::execute(GameObject* gameObject, float deltaTime) {
-	if (m_target = nullptr)
+	if (m_target == nullptr)
 	{
 		return eBehaviourResult::FAILURE;
 	}
 
+	float x, y;
+	float target_x, target_y;
+
+	gameObject->getPosition(&x, &y);
+	m_target->getPosition(&target_x, &target_y);
+
+
+
+	return eBehaviourResult::SUCCESS;
 }
